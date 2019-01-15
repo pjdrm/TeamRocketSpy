f9d0c4f5922b3860b8252f7a762c1c4006985300
pjdrm <pjdrm@LindaMartini>
2019-01-15 18:14:55 +0000

    Updating scraping PoGo events.

--- src/pogo_events_scrapper.py
+++ src/pogo_events_scrapper.py
@@ -27,32 +27,49 @@ class PogoEventsScrapper():
     def scrape_pogo_events(self):
         self.driver.get('https://p337.info/pokemongo/')
         pogo_event_list = []
+        event_links = []
         for i in range(1, 10):
-            event_icon = self.driver.find_elements_by_xpath('//*[@id="container_right"]/a['+str(i)+']/div/div[@class="extra_timer_left"]/img')
-            if len(event_icon) == 0:
+            event = self.driver.find_elements_by_xpath('//*[@id="container_right"]/a['+str(i)+']')
+            if len(event) == 0:
                 break
-            event_icon = event_icon[0].get_attribute("src")
-            event_desc = self.driver.find_elements_by_xpath('//*[@id="container_right"]/a['+str(i)+']/div/div[@class="extra_timer_right"]')[0].text
-            split_str = event_desc.split("\n")
-            if len(split_str) == 3:
-                date_split = split_str[2].split(" ")
-            elif len(split_str) == 1:
-                continue #seems to be the case where they have an event but no date
             else:
-                date_split = split_str[1].split(" ")
+                event_link = event[0].get_attribute("href")
+                if "nest-migration" in event_link:
+                    break
+                event_links.append(event_link)
+                
+        for event_link in event_links:
+            self.driver.get(event_link)
+            event_desc = self.driver.find_elements_by_xpath('//*[@id="text"]/div[2]')
+            if len(event_desc) > 1:
+                for ed in event_desc:
+                    if "Europe" in ed.get_attribute("innerText"):
+                        event_desc = ed.get_attribute("innerText")
+                        break
+            else:
+                event_desc = event_desc[0].get_attribute("innerText")
+            if "Your Local Time:" not in event_desc:
+                continue
+            split_str = event_desc.split("Your Local Time:")
+            date = split_str[1].strip()
+            date_split = date.split(" ")
             date = date_split[0]+" "+date_split[1]+" "+date_split[4]
             event_end_date = datetime.strptime(date[4:], '%d-%b-%Y (%H:%M)')
             present = datetime.now()
             if event_end_date < present:
                 continue #Event is over
-                
-            if ("Starts" in event_desc) or ("Start" in event_desc):
-                desc = split_str[0].replace(" Starts", "").replace(" Start", "")
+            
+            if "Europe" in split_str[0]:
+                event_name = (split_str[0].split(" in Europe")[0].replace("starts", "Starts")+":").strip()
+            else:
+                event_name = split_str[0].split("\n")[0]
+            if ("Start" in event_name):
+                desc = event_name.replace(" Starts", "").replace(" Start", "")
                 date = "Starts: "+date
             else:
-                desc = split_str[0].replace(" Ends", "").replace(" End", "")
+                desc = event_name.replace(" Ends", "").replace(" End", "")
                 date = "Ends: "+date
-            pogo_event_list.append({"icon": event_icon, "desc": desc, "date": date})
+            pogo_event_list.append({"desc": desc, "date": date})
         return pogo_event_list
             
-#PogoEventsScrapper()
+PogoEventsScrapper().scrape_pogo_events()
