c2adfa471b880d1c160574a596577272c2a4744c
pjdrm <pjdrm@PedroMint>
2019-01-29 14:41:15 +0000

    Adding quest goals.

--- src/monocle_scrapper.py
+++ src/monocle_scrapper.py
@@ -32,6 +32,108 @@ def get_pokestop_name(guid, cnx):
     cursor.execute(query)
     return cursor.fetchone()[0].strip()
 
+def get_quest_goal(quest_type, quest_target, quest_condition):
+    quest_goal = None
+    if quest_type == 4:
+        #Catch Pokemon quests
+        quest_goal = "Catch "+str(quest_target)+" "
+        if len(quest_condition) == 0:
+            quest_goal += "Pokemon"
+        elif "with_pokemon_category" in quest_condition[0] and "pokemon_ids" in quest_condition[0]["with_pokemon_category"]:
+            catch_mon_desc = ""
+            prev_id = -1
+            for pokemon_id in quest_condition[0]["with_pokemon_category"]["pokemon_ids"]:
+                if pokemon_id == (prev_id+1):
+                    prev_id = pokemon_id
+                    continue
+                pokemon_name = POKE_INFO[str(pokemon_id)]["name"]
+                catch_mon_desc += pokemon_name+" "
+                prev_id = pokemon_id
+            str_split = catch_mon_desc.split(" ")
+            str_split[-1] = "or "+str_split[-1]
+            for mon_name in str_split:
+                quest_goal += mon_name+" "
+            quest_goal += "Pokemon"
+        elif "with_pokemon_type" in quest_condition[0] and "pokemon_type" in quest_condition[0]["with_pokemon_type"]:
+            type_mon_desc = ""
+            for pokemon_type in quest_condition[0]["with_pokemon_type"]["pokemon_type"]:
+                type_desc = TYPE_DICT[pokemon_type]
+                type_mon_desc += type_desc+", "
+            type_mon_desc = type_mon_desc[:-2]
+            str_split = type_mon_desc.split(" ")
+            str_split[-1] = "or "+str_split[-1][1:]
+            for mon_type in str_split:
+                quest_goal += mon_type
+            quest_goal += "Pokemon"
+        elif quest_condition[0]["type"] == 3:
+            quest_goal = "weather boosted Pokemon"
+    elif quest_type == 16:
+        #Throw quests
+        if quest_condition[0]["type"] == 8 and quest_condition[0]["with_throw_type"]["throw_type"] == 10:
+            quest_goal = "Make "+str(quest_target)+" nice throws"
+        elif quest_condition[0]["type"] == 8 and quest_condition[0]["with_throw_type"]["throw_type"] == 11:
+            quest_goal = "Make "+str(quest_target)+" great throws"
+        elif quest_condition[0]["type"] == 14 and quest_condition[0]["with_throw_type"]["throw_type"] == 10:
+            quest_goal = "Make "+str(quest_target)+" nice throws in row"
+        elif quest_condition[0]["type"] == 14 and quest_condition[0]["with_throw_type"]["throw_type"] == 11:
+            quest_goal = "Make "+str(quest_target)+" great throws in a row"
+    elif quest_type == 8:
+        #Raid quests
+        if len(quest_condition[0]) == 0:
+            quest_goal = "Battle in a raid"
+        elif quest_target == 1:
+            quest_goal = "Win a raid"
+        else:
+            quest_goal = "Win "+str(quest_target)+" raids"
+    elif quest_type == 13:
+        #Berry quests
+        if quest_condition[0]["with_item"]["item"] == 705:
+            quest_goal = "Use "+str(quest_target)+" Pinap Berries while catching Pokemon"
+        elif quest_condition[0]["with_item"]["item"] == 701:
+            quest_goal = "Use "+str(quest_target)+" Razz Berries to help catch Pokemon"
+    elif quest_type == 24:
+        #Gift quests
+        quest_goal = "Send "+str(quest_target)+" to friends"
+    elif quest_type == 14:
+        #Power up quest
+        quest_goal = "Power up a Pokemon "+str(quest_target)+" times"
+    elif quest_type == 7:
+        #Gym battle quests
+        if len(quest_condition[0]) > 0 and quest_condition[0]["type"] == 10:
+            quest_goal = "Use a supereffective charged attack in "+str(quest_target)+" gym battles"
+        else:
+            if quest_target > 1:
+                quest_goal = "Battle in a gym "+str(quest_target)+" times"
+            else:
+                quest_goal = "Battle in a gym"
+    elif quest_type == 15:
+        #Evolve quests
+        if len(quest_condition[0]) > 0:
+            if quest_condition[0]["type"] == 11:
+                quest_goal = "Use an item to evolve a Pokemon"
+            elif "with_pokemon_category" in quest_condition[0]:
+                pokemon_name = POKE_INFO[str(quest_condition[0]["with_pokemon_category"][0])]["name"]
+                quest_goal = "Evolve "+str(quest_target)+" "+pokemon_name
+        elif quest_target == 1:
+            quest_goal = "Evolve a Pokemon"
+        else:
+            quest_goal = "Evolve "+str(quest_target)+" Pokemons"
+    elif quest_type == 10:
+        quest_goal = "Transfer "+str(quest_target)+" Pokemons"
+    elif quest_type == 6:
+        #Hatch eggs quests
+        if quest_target == 1:
+            quest_goal = "Hatch an egg"
+        else:
+            quest_goal = "Hatch "+str(quest_target)+" eggs"
+    elif quest_type == 23:
+        #Trade quests
+        if quest_target == 1:
+            quest_goal = "Trade a Pokemon"
+        else:
+            quest_goal = "Trade "+str(quest_target)+" Pokemons"
+    return quest_goal             
+    
 def is_present_raid(raid_info):
     raid_end_time = None
     if raid_info["raid_starts_in"] is not None:
@@ -171,12 +273,12 @@ def scrape_monocle_quests(config):
     cnx = mysql.connector.connect(**db_config)
     cursor = cnx.cursor(buffered=True)
     
-    query = "SELECT GUID, quest_timestamp, quest_stardust, quest_pokemon_id, quest_item_id, quest_item_amount FROM trs_quest"
+    query = "SELECT GUID, quest_timestamp, quest_stardust, quest_pokemon_id, quest_item_id, quest_item_amount, quest_type, quest_target, quest_condition FROM trs_quest"
     cursor.execute(query)
     
     quest_list = []
     
-    for (GUID, quest_timestamp, quest_stardust, quest_pokemon_id, quest_item_id, quest_item_amount) in cursor:
+    for (GUID, quest_timestamp, quest_stardust, quest_pokemon_id, quest_item_id, quest_item_amount, quest_type, quest_target, quest_condition) in cursor:
         quest_timestamp = datetime.datetime.fromtimestamp(quest_timestamp).strftime("%Y-%m-%d")
         current_timestamp =dt.now().strftime("%Y-%m-%d")
         if quest_timestamp != current_timestamp:
@@ -193,7 +295,8 @@ def scrape_monocle_quests(config):
         if pokestop == "unknown":
             print("MAD has not picked up Pokestop name")
             continue
-        quest_list.append({"pokestop": pokestop, "reward": reward})
+        quest_goal = get_quest_goal(quest_type, quest_target, quest_condition)
+        quest_list.append({"pokestop": pokestop, "reward": reward, "goal": quest_goal})
     quest_list = sorted(quest_list, key=lambda k: k["reward"]) 
     return quest_list
             
@@ -202,6 +305,25 @@ GYMS_INFO = "./config/gym_info.json"
 MOVE_DICT = load_move_protos("./config/proto_moves.json")
 with open("./config/pokemon.json") as data_file:    
     POKE_INFO = json.load(data_file)
+    
+TYPE_DICT = {1: "Normal",
+            2: "Fighting",
+            3: "Flying",
+            4: "Poison",
+            5: "Ground",
+            6: "Rock",
+            7: "Bug",
+            8: "Ghost",
+            9: "Steel",
+            10: "Fire",
+            11: "Water",
+            12: "Grass",
+            13: "Electric",
+            14: "Psychic",
+            15: "Ice",
+            16: "Dragon",
+            17: "Dark",
+            18: "Fairy"}
         
 with open("./config/proto_items.json") as data_file:    
     ITEMS_DICT = json.load(data_file)
