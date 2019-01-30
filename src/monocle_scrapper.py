6c9650c0b98fd35ca6cba5d44bf0dea0d6aa26fb
pjdrm <pjdrm@LindaMartini>
2019-01-30 13:50:27 +0000

    Missing berry quest.

--- src/monocle_scrapper.py
+++ src/monocle_scrapper.py
@@ -99,7 +99,9 @@ def get_quest_goal(quest_type, quest_target, quest_condition):
         quest_goal = "Transfer "+str(quest_target)+" Pokemons"
     elif quest_type == 13:
         #Berry quests
-        if quest_condition[0]["with_item"]["item"] == 705:
+        if "with_item" not in quest_condition[0]:
+            quest_goal = "Use "+str(quest_target)+" to help catch Pokemon"
+        elif quest_condition[0]["with_item"]["item"] == 705:
             quest_goal = "Use "+str(quest_target)+" Pinap Berries while catching Pokemon"
         elif quest_condition[0]["with_item"]["item"] == 701:
             quest_goal = "Use "+str(quest_target)+" Razz Berries to help catch Pokemon"
