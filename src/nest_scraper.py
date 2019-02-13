bf4659a1029ae2d1855f30f04035116ece19ff2d
pjdrm <pjdrm@LindaMartini>
2019-02-13 20:10:34 +0000

    First working version of nest scraper.

--- src/nest_scraper.py
+++ src/nest_scraper.py
@@ -9,45 +9,38 @@ import mysql.connector
 from collections import Counter
 import operator
 import json
+import datetime
+from datetime import datetime as dt
 
 def load_geofences(file_path):
-    nests = []
-    polygon = None
-    gf_name = None
     with open(file_path) as f:
-        lins = f.readlines()
-        for l in lins:
-            if "[" in l:
-                if polygon is not None:
-                    nests.append({"name": gf_name, "polygon": polygon})
-                gf_name = l[1:-2]
-                polygon = []
-            else:
-                lat, lon = l.split(",")
-                lat = float(lat)
-                lon = float(lon)
-                polygon.append((lat, lon))
-        nests.append({"name": gf_name, "polygon": polygon})
-    return nests
+        nests = eval(f.read())
+        mon_black_list =nests["mon_black_list"]
+        nests.pop("mon_black_list")
+        for name in nests:
+            polygon_pts = nests[name]["polygon"]
+            polygon = Polygon(polygon_pts)
+            nests[name]["polygon"] = polygon
+    return nests, mon_black_list
     
-def inside_geofence(geofence, point):
+def inside_geofence(polygon, point):
     lat = point[0]
     lon = point[1]
-    polygon = Polygon(geofence) # create polygon
     point = Point(lat, lon) # create point
     is_inside = point.within(polygon)
     return is_inside
     
 def find_geofence(geofences, point):
-    for geofence in geofences:
-        if inside_geofence(geofence, point):
-            return geofence
+    for name in geofences:
+        polygon = geofences[name]["polygon"]
+        if inside_geofence(polygon, point):
+            return name
     return None
 
 def find_nesting_mon(spawns, black_mon_list):
     spawn_counts = Counter(spawns)
-    ordered_spawn_counts = sorted(spawn_counts.items(), key=lambda kv: kv[1])
-    for mon_id in ordered_spawn_counts:
+    ordered_spawn_counts = spawn_counts.most_common(30)
+    for mon_id, mon_count in ordered_spawn_counts:
         mon_name = POKE_INFO[str(mon_id)]["name"]
         if mon_name not in black_mon_list:
             return mon_name
@@ -57,13 +50,15 @@ def find_nesting_mon(spawns, black_mon_list):
 def assign_spawns(geofences, spawns):
     nests = {}
     for spawn in spawns:
-        target_geofence = find_geofence(geofences, spawn["point"])
-        if target_geofence is None:
+        gf_name = find_geofence(geofences, spawn["point"])
+        if gf_name is None:
             continue
-        gf_name = target_geofence["name"]
         if gf_name not in nests:
             nests[gf_name] = []
         nests[gf_name].append(spawn["pokemon_id"])
+    for name in geofences:
+        if name not in nests:
+            print("WARNING: no spawn points for nest %s"%name)
     return nests
 
 def get_spawns(config):
@@ -76,23 +71,33 @@ def get_spawns(config):
     cnx = mysql.connector.connect(**db_config)
     cursor = cnx.cursor(buffered=True)
     
-    query = "SELECT pokemon_id, lat, lon FROM sightings"
+    query = "SELECT pokemon_id, lat, lon, updated FROM sightings"
     cursor.execute(query)
     
     spawns = []
-    for (pokemon_id, lat, lon) in cursor:
+    current_timestamp = "2019-02-09"#dt.now().strftime("%Y-%m-%d")
+    for (pokemon_id, lat, lon, updated) in cursor:
+        timestamp_timestamp = datetime.datetime.fromtimestamp(updated).strftime("%Y-%m-%d")
+        if timestamp_timestamp != current_timestamp:
+            continue
         spawns.append({"pokemon_id": pokemon_id, "point": (lat, lon)})
     return spawns
+
+def find_nests(tr_spy_config):
+    nest_config_path = tr_spy_config["nest_config_path"]
+    geofences, mon_black_list = load_geofences(nest_config_path)
+    spawns = get_spawns(tr_spy_config)
+    nests = assign_spawns(geofences, spawns)
+    for name in nests:
+        nestig_mon = find_nesting_mon(nests[name], mon_black_list)
+        if nestig_mon is not None:
+            print("%s is a %s nest"%(name, nestig_mon))
+    
 
 with open("./config/pokemon.json") as data_file:    
     POKE_INFO = json.load(data_file)
         
         
-'''
-geofence = [(38.7365052738808, -9.1430676271793), (38.7365471180395, -9.1417372515079), (38.7352750446586, -9.1414797594425), (38.7351997238137, -9.1426599314091)]
-inside_point = (38.7362081, -9.1423313)
-outside_point = (38.7350532, -9.143651)
-inside_geofence(geofence, inside_point)
-inside_geofence(geofence, outside_point)
-'''
-print(load_geofences("/home/pjdrm/workspace/TeamRocketSpy/config/nests_geofences.txt"))
+with open("/home/pjdrm/eclipse-workspace/TeamRocketSpy/config/tr_spy_config.json") as data_file:    
+    tr_spy_config = json.load(data_file)
+find_nests(tr_spy_config)
