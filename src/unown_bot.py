cd19e31f03bc390e2bfa08b05952c4c231442733
pjdrm <pjdrm@LindaMartini>
2019-02-21 15:44:13 +0000

    Changing attack info embed looking.

--- src/unown_bot.py
+++ src/unown_bot.py
@@ -348,10 +348,10 @@ class UnownBot():
         else:   
             gym_info = "<:instinct:399568286033117197>"
         gym_info = "Team "+team+gym_info
-        moveset_embed=discord.Embed(title="**Raid Info**:", color=SIDEBAR_EMBED_COLOR)
-        moveset_embed.add_field(name="Fast", value=fast_attack, inline=True)
-        moveset_embed.add_field(name="Charge", value=charge_attack, inline=True)
-        moveset_embed.add_field(name="Gym control", value=gym_info, inline=False)
+        moveset_embed=discord.Embed(title="**Raid Info**", color=SIDEBAR_EMBED_COLOR)
+        moveset_embed.add_field(name="Boss Attacks", value=fast_attack, inline=True)
+        moveset_embed.add_field(name="", value=charge_attack, inline=False)
+        moveset_embed.add_field(name="Gym Control", value=gym_info, inline=False)
         moveset_embed.set_footer(text="Requested by "+user, icon_url=user_icon)
         await gym_channel.send(embed=moveset_embed)
         
